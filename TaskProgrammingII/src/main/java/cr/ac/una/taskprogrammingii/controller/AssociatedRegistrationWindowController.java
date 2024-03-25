77cb3699cb05358b3c2ff9f6dadbd4ef8d044376
Greichel Campos Jiménez <greichel.campos.jimenez@est.una.ac.cr>
2024-03-25 15:03:31 -0600

    Fix pom bugs and add plugin
    Se le modifico las verciones en el pom de la tarea y le agregue un plugin

--- TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/AssociatedRegistrationWindowController.java
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/AssociatedRegistrationWindowController.java
@@ -17,6 +17,8 @@ public class AssociatedRegistrationWindowController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {

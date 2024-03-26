66448f5d23486812f7e92e6ec3cd3341a6f8a582
SofiaBejaranoMora <aslyn.bejarano.mora@est.una.ac.cr>
2024-03-26 16:13:43 -0600

    Development of associatedRegistrationView
    The associatedRegistrationView components have been created and the buttons and screen texts are being configured in the controller

--- TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/AssociatedRegistrationController.java
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/AssociatedRegistrationController.java
@@ -4,14 +4,20 @@
  */
 package cr.ac.una.taskprogrammingii.controller;
 
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
+
 import javafx.fxml.Initializable;
 
+import java.awt.event.MouseEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -19,16 +25,53 @@ import javafx.scene.control.PasswordField;
  * @author Sofia Bejarano Mora
  */
 public class associatedRegistrationController implements Initializable {
-
-        @FXML
-    private PasswordField pssFolio;
+    Boolean activatedEnterFolio= true;
+    
+ @FXML
+    private Button btnIncome;
 
     @FXML
-    private Label txtIncomeChange;
+    private Button btnIncomeChange;
 
+    @FXML
+    private PasswordField pstFolio;
+
+    @FXML
+    private AnchorPane root;
+
+      @FXML
+    private Label labIncome;
+
+    @FXML
+    private Label labIncomeChange;
+
+    @FXML
+    void onActionBtnIncome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnIncomeChange(ActionEvent event) {
+        activatedEnterFolio=!activatedEnterFolio;
+       if(activatedEnterFolio){
+           activatedEnterFolio=false;
+            labIncomeChange.setText("Ocupamos que nos des una pequeña ayuda, nos podrias dar tu nombre completo?");
+        }
+       else if (!activatedEnterFolio){
+           activatedEnterFolio=true;
+            labIncome.setText("Ocupamos que nos des una pequeña ayuda, nos podrias dar tu nombre completo.");
+            labIncomeChange.setText("¿Prefieres ingresar tu folio?");
+        }
+    }
+
+    @FXML
+    void onActionPstFolio(ActionEvent event) {
+
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }

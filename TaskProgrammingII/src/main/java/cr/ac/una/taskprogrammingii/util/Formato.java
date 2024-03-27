f4b6f590f71fd35bc2de7d5516b96da20f393d68
Greichel Campos Jiménez <greichel.campos.jimenez@est.una.ac.cr>
2024-03-27 10:40:04 -0600

    Fix small errors
    Correcting errors on changes of direction

--- TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/util/Formato.java
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/util/Formato.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cr.ac.una.unaplanillaj21.util;
+package cr.ac.una.taskprogrammingii.util;
 
 import java.text.DecimalFormat;
 import java.text.ParsePosition;

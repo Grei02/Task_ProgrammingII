bd4753d372da4eb25ebe656132025bd857fa80ed
Greichel Campos Jiménez <greichel.campos.jimenez@est.una.ac.cr>
2024-04-03 18:06:26 -0600

    Fix error with "principal view"

--- TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/util/FlowController.java
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/util/FlowController.java
@@ -83,7 +83,7 @@ public class FlowController {
 
     public void goMain(String nameView) {
         try {
-            this.mainStage.setScene(new Scene(FXMLLoader.load(App.class.getResource("view/"+nameView+".fxml"), this.idioma)));
+            this.mainStage.setScene(new Scene(FXMLLoader.load(App.class.getResource("view/PrincipalView.fxml"), this.idioma)));
             MFXThemeManager.addOn(this.mainStage.getScene(), Themes.DEFAULT, Themes.LEGACY);
             this.mainStage.show();
         } catch (IOException ex) {

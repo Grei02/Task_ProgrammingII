adb87955b393dade79c43843ebdd5c1973769638
SofiaBejaranoMora <aslyn.bejarano.mora@est.una.ac.cr>
2024-04-20 14:30:38 -0600

    the transfer window was created

---
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/TransferStudentController.java
@@ -0,0 +1,84 @@
+package cr.ac.una.taskprogrammingii.controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class TransferStudentController extends Controller implements Initializable {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private MFXComboBox<?> cmbAccount;
+
+    @FXML
+    private MFXComboBox<?> cmbTransferType;
+
+    @FXML
+    private TextField txtAccountAmount;
+
+    @FXML
+    private TextField txtAmountTransfer;
+
+    @FXML
+    private TextField txtFolio;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void onActionBtnCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCmbAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCmbTransferType(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionTxtAmountTransfer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionTxtFolio(ActionEvent event) {
+
+    }
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+    }    
+
+    @Override
+    public void initialize() {
+        
+    }
+    
+}

76f3f2183a9cb0878a75266e37472ad70695d9ec
Greichel Campos Jiménez <greichel.campos.jimenez@est.una.ac.cr>
2024-04-21 23:24:00 -0600

    Add new View

---
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/UserMaintenanceController.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package cr.ac.una.taskprogrammingii.controller;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author USUARIO PZ UNA
+ */
+public class UserMaintenanceController implements Initializable {
+
+    @FXML
+    private ImageView imgUserImage;
+    @FXML
+    private MFXButton btnExit;
+    @FXML
+    private MFXButton btbSave;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onImageClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnExit(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnSave(ActionEvent event) {
+    }
+    
+}

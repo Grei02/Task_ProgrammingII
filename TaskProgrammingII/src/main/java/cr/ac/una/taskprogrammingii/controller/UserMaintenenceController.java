04f9ecac33943a0bd2fa2cc58a6e694c36fcdca3
Greichel Campos Jiménez <greichel.campos.jimenez@est.una.ac.cr>
2024-04-23 00:17:00 -0600

    Fix errorrs

--- TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/UserMaintenenceController.java
+++ TaskProgrammingII/src/main/java/cr/ac/una/taskprogrammingii/controller/UserMaintenenceController.java
@@ -4,23 +4,82 @@
  */
 package cr.ac.una.taskprogrammingii.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
  *
  * @author USUARIO PZ UNA
  */
-public class UserMaintenenceController implements Initializable {
+public class UserMaintenenceController extends Controller  implements Initializable {
+
+    @FXML
+    private MFXTextField txtFoil;
+    @FXML
+    private MFXButton btnSearch;
+    @FXML
+    private MFXButton btnExit;
+    @FXML
+    private MFXButton btnSave;
+    @FXML
+    private ImageView imgUser;
+    
+    Boolean isFoil=false;
+    
 
     /**
      * Initializes the controller class.
      */
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionBtnSearch(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void onImageClicked(MouseEvent event) {
+        if(isFoil){
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Archivos de imagen", "*.png", "*.jpg", "*.gif");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File selectedFile = fileChooser.showOpenDialog(null);
+       
+        if (selectedFile != null) {
+            Image newImage = new Image(selectedFile.toURI().toString());
+            imgUser.setImage(newImage);
+        }
+        }
+        else{
+        }
+    }
+
+    @FXML
+    private void onActionBtnExit(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnSave(ActionEvent event) {
+    }
+
+    @Override
+    public void initialize() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
